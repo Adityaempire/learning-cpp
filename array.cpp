7da6eaeb34ad957b7f4fcd04e0f5536a25befd58
Adityaempire <106740072+Adityaempire@users.noreply.github.com>
2022-06-19 13:00:02 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    int adi[] = {1,3,5,6,9};
+    char adi2[]= {'a','b','c'};
+    
+    std::cout << adi[3]<<std::endl;
+    std::cout << adi2[1];
+    return 0;
+}
